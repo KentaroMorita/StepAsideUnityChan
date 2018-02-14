5b072504af4ffb8e5a6a06b68f5836cf37910ef5
KentaroMorita <moriken.ppi.1031@gmail.com>
2018-02-15 03:55:40 +0900

    StepAsideUnityChanプロジェクト　初コミット
    Lesson6基本課題の部分です

---
+++ Assets/ItemDestroyer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDestroyer : MonoBehaviour {
+	
+	private GameObject unityChan;
+
+	// Use this for initialization
+	void Start () {
+		this.unityChan = GameObject.Find("unitychan");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//unityちゃんとオブジェクトの距離比較
+		if (this.transform.position.z - unityChan.transform.position.z < -20) {
+			Destroy(this.gameObject);
+		}
+	}
+}
